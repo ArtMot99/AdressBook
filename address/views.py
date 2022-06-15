2cfb07e7660069a429db4afa9f7ded03d6af0941
Artem <104223700+ArtMot99@users.noreply.github.com>
2022-06-15 21:46:26 +0200

    Добавлено представление(используя шаблон index.html)

--- address/views.py
+++ address/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Contact, Phone, Email
 
-# Create your views here.
+
+def index(request):
+    context = {}
+    qs_contact = Contact.objects.all()
+    qs_phone = Phone.objects.all()
+    qs_email = Email.objects.all()
+    context['contact'] = qs_contact
+    context['phone_number'] = qs_phone
+    context['email'] = qs_email
+
+    return render(request, 'index.html', context)
