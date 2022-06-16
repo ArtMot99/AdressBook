6b0cc565cae3279971bcca204749c076bf93e0d4
Artem <104223700+ArtMot99@users.noreply.github.com>
2022-06-16 20:33:17 +0200

    Добавлен переход на роут с помощью параметра name в urls.

--- address/urls.py
+++ address/urls.py
@@ -4,5 +4,5 @@ from address import views
 
 urlpatterns = [
     path('', views.index, name='main_menu'),
-    path('<int:pk>', views.description_contact, name='primery_key'),
+    path('<int:pk>', views.description_contact, name='info'),
 ]
