5947c582716a3a843012307c61dd1118fb793c44
Artem <104223700+ArtMot99@users.noreply.github.com>
2022-06-16 21:23:39 +0200

    Поправлены шаблоны, добален вывод номеров телефона для каждого контакта, при просмотре полного инфо о контакте.

--- address/views.py
+++ address/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .models import Contact, Phone, Email
 
@@ -6,16 +5,17 @@ from .models import Contact, Phone, Email
 def index(request):
     context = {}
     qs_contact = Contact.objects.all()
-    qs_phone = Phone.objects.all()
-    qs_email = Email.objects.all()
     context['contact'] = qs_contact
-    context['phone_number'] = qs_phone
-    context['email'] = qs_email
-
     return render(request, 'index.html', context)
 
 
 def description_contact(request, pk):
+    context = {}
     obj = get_object_or_404(Contact, pk=pk)
+    context['contact'] = obj
+    qs_phone = Phone.objects.all()
+    context['phone'] = qs_phone
+    qs_email = Email.objects.all()
+    context['email'] = qs_email
 
-    return render(request, 'info_about_contact.html', {'contact': obj})
+    return render(request, 'info_about_contact.html', context)
