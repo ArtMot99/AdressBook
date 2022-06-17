d37d0c6b07cf19bab5c24ef7cfa3b1dac37dc7a4
Artem <104223700+ArtMot99@users.noreply.github.com>
2022-06-17 20:30:44 +0200

    Вывод для каждого контакта номера телефона и емайл адресса, реализован через шаблон.

--- address/views.py
+++ address/views.py
@@ -13,9 +13,5 @@ def contact_view(request, pk):
     context = {}
     obj = get_object_or_404(Contact, pk=pk)
     context['contact'] = obj
-    qs_phone = Phone.objects.all()
-    context['phone'] = qs_phone
-    qs_email = Email.objects.all()
-    context['email'] = qs_email
 
     return render(request, 'info_about_contact.html', context)
