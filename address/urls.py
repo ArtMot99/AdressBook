8ec96deebbe495a331deab12828b8c713da454da
Artem <104223700+ArtMot99@users.noreply.github.com>
2022-06-21 14:46:17 +0200

    Создано представление для update contact

--- address/urls.py
+++ address/urls.py
@@ -5,5 +5,7 @@ from address import views
 urlpatterns = [
     path('', views.contact_list_view, name='main_menu'),
     path('<int:pk>/', views.contact_view, name='info'),
+    path('<int:pk>/update/', views.update_contact_modelform_view, name='update'),
     path('create/', views.create_contact_modelform_view, name='create'),
+
 ]
--- address/views.py
+++ address/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
-from .forms import CreateContactModelForm
+from .forms import CreateContactModelForm, PhoneForm
 from .models import Contact
 
 
@@ -14,12 +14,12 @@ def contact_list_view(request):
 
 def contact_view(request, pk):
     obj = get_object_or_404(Contact, pk=pk)
-    phone_numbers = [number.phone_number for number in obj.phone_set.all()]
-    emails_address = [email.email_address for email in obj.email_set.all()]
+    # phone_numbers = [number.phone_number for number in obj.phone_set.all()]
+    # emails_address = [email.email_address for email in obj.email_set.all()]
     context = {
         'contact': obj,
-        'phone_numbers': phone_numbers,
-        'emails': emails_address,
+        # 'phone_numbers': phone_numbers,
+        # 'emails': emails_address,
     }
 
     return render(request, 'address/info_about_contact.html', context)
@@ -28,14 +28,41 @@ def contact_view(request, pk):
 def create_contact_modelform_view(request, *args, **kwargs):
     if request.method == 'POST':
         form = CreateContactModelForm(request.POST)
-        if form.is_valid():
+        phone_form = PhoneForm(request.POST)
+        if form.is_valid() and phone_form.is_valid():
             contact = form.save(commit=False)
             contact.user = request.user
             contact.save()
+            phone = phone_form.save(commit=False)
+            phone.contact = contact
+            phone.save()
             return HttpResponseRedirect(reverse('main_menu'))
         else:
-            return render(request, 'address/create_contact.html', context={'form': form})
+            return render(request, 'address/create_contact.html', context={'form': form,
+                                                                           'phone_form': phone_form})
     else:
         form = CreateContactModelForm()
-    return render(request, 'address/create_contact.html', context={'form': form})
+        phone_form = PhoneForm()
+    return render(request, 'address/create_contact.html', context={'form': form,
+                                                                   'phone_form': phone_form})
+
+
+def update_contact_modelform_view(request, pk, *args, **kwargs):
+    obj = get_object_or_404(Contact, pk=pk)
+    if request.method == 'POST':
+        form = CreateContactModelForm(data=request.POST, instance=obj)
+        phone_form = PhoneForm(data=request.POST, instance=obj.phone_set.first())
+        if form.is_valid() and phone_form.is_valid():
+            contact = form.save()
+            phone = phone_form.save()
+            return HttpResponseRedirect(reverse('info', kwargs={'pk': pk}))
+        else:
+            return render(request, 'address/create_contact.html', context={'form': form,
+                                                                           'phone_form': phone_form})
+    else:
+        form = CreateContactModelForm(instance=obj)
+        phone_form = PhoneForm(instance=obj.phone_set.first())
+    return render(request, 'address/create_contact.html', context={'form': form,
+                                                                   'phone_form': phone_form})
+
 
