4eee2c948000364168263250fe787780c85a18f6
Artem <104223700+ArtMot99@users.noreply.github.com>
2022-07-10 14:15:12 +0200

    Добавлена возможность добавления комментов через админку

--- address/admin.py
+++ address/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from address.models import Contact, Phone, Email
-
+from address.models import Contact, Phone, Email, Comment
 
 admin.site.register(Contact)
 admin.site.register(Phone)
 admin.site.register(Email)
+admin.site.register(Comment)
